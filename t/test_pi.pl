555bf26c54a0b47eed2df803c641e06a23157cb4
KjetilS <k.a.skotheim@ceres.no>
2017-04-14 13:47:03 +0200

    pi-madness

---
+++ t/test_pi.pl
@@ -0,0 +1,21 @@
+use Acme::Tools;
+print "$Acme::Tools::PI\n";
+my $pi=bigf($PI);
+for(1..8){
+    my $t=time_fp();
+    my $to=10**$_;
+    my $sum=bigf(0);
+    my $e;
+    $e=(bigf(-1)**$_)/(2*$_+1) and $sum+=$e #and printf"%d%20.15f%20.15f\n",$_,$e,$sum
+	for 0..$to;
+    my $mypi=4*$sum;
+    printf "%10d:  %27.22f %27.22f %27.22f %27.22f %27.22f    %5.2fs\n",
+      $to,
+      $mypi,
+      $pi-$mypi,
+      $pi-($mypi - 1/$to**1),
+      $pi-($mypi - 1/$to**1 + 1/$to**2),
+      $pi-($mypi - 1/$to**1 + 1/$to**2 - 2/$to**3),
+     #$pi-($mypi - 1/$to**1 + 1/$to**2 - 2/$to**3 - 2/$to**6),
+      time_fp()-$t;
+}
